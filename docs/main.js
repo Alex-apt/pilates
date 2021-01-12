b97051670dabb95ce9ee665949db7e3e54212954
Manuel Alejandro <manuelaleja1980@gmail.com>
2021-01-12 13:09:03 +0100

    subiendo projecto

---
+++ docs/main.js
@@ -0,0 +1,52 @@
+
+/* ********** Menu ********** */
+((d) => {
+    const $btnMenu = d.querySelector(".menu-btn"),
+      $menu = d.querySelector(".menu");
+  
+    $btnMenu.addEventListener("click", (e) => {
+      $btnMenu.firstElementChild.classList.toggle("none");
+      $btnMenu.lastElementChild.classList.toggle("none");
+      $menu.classList.toggle("is-active");
+    });
+  
+    d.addEventListener("click", (e) => {
+      if (!e.target.matches(".menu a")) return false;
+  
+      $btnMenu.firstElementChild.classList.remove("none");
+      $btnMenu.lastElementChild.classList.add("none");
+      $menu.classList.remove("is-active");
+    });
+  })(document);
+
+  /* Carousel */
+  
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
